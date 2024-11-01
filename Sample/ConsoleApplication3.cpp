c658adc7795f3f07f2aed843ebe5b296820199c4
CellarCordial <162788237+CellarCordial@users.noreply.github.com>
2024-11-01 17:56:21 +0800

    Update ConsoleApplication3.cpp

--- Sample/ConsoleApplication3.cpp
+++ Sample/ConsoleApplication3.cpp
@@ -9,12 +9,10 @@ int main()
 	FSampleDesc SampleDesc{ 1 };
 	FSampleDesc* pSampleDesc = &SampleDesc;
 
-	// 创建对象
 	CreateSampleInstance(IID_ITest1, PPV_ARG(pTest1.GetAddressOf()), (void*)pSampleDesc);
 
 	pTest1->SomeFunction1();
 
-	// 访问其它接口
 	ITest2* pTest2 = nullptr;
 	pTest1->QueryInterface(IID_ITest2, (void**)&pTest2);
 
@@ -23,13 +21,11 @@ int main()
 	pTest1Ref->SomeFunction1();
 	pTest2->SomeFunction2();
 
-	// 访问聚合对象的接口
 	ITest3* pTest3;
 	pTest2->QueryInterface(IID_ITest3, PPV_ARG(&pTest3));
 
 	pTest3->SomeFunction3();
 
-	// 从聚合对象接口里访问外部对象的接口
 	ITest2* pTest2Ref = nullptr;
 	pTest3->QueryInterface(IID_ITest2, PPV_ARG(&pTest2Ref));
 	pTest2Ref->SomeFunction2();
